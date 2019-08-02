2a76cba2e2fb4bdad5c8a178354293be4ec12e4a
unknown <Alistair>
2019-08-02 00:05:43 -0400

    Begin building out logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,42 @@
 console.log("hello world!");
 
-$("#userTextInput")
-$("#addNote")
+// // Text box
+// // Add note button
+
+function showNewNote(list) {
+  // Add new note div
+  $("newNoteDiv").empty();
+  
+  for (let i = 0; i < list.length; i++) {
+
+    let note = $('<p>');
+    note.text(list[i]);
+
+    let closeNote = $("<button>");
+    closeNote.attr("new-note", i);
+    closeNote.addClass("delete");
+    closeNote.text("X");
+
+    note = note.prepend(closeNote);
+    $("#newNoteDiv").append(note);
+  }
+}
+
+$("#addNote").on("click", function(event){
+    event.preventDefault();
+
+    let newNote = $("#userTextInput").val().trim();
+    list.push(newNote);
+
+    showNewNote(list);
+
+    localStorage.setItem("note-added", JSON.stringify(list));
+
+    $("#userTextInput").val("");
+});
+
+$(document).on("click", ".delete", function() {
+
+    let noteNumber = $(this).attr("new-note");
+});
+
